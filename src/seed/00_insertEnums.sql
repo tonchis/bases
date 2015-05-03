9bd5ae07b9ee9d7802a3f5740ff8f963bffd7908
Pablo Rago <p4blo.r@gmail.com>
2015-05-03 17:16:33 -0300

    Data para TipoEleccion y TipoTerritorio + Rename de inserPersona

---
+++ src/seed/00_insertEnums.sql
@@ -0,0 +1,3 @@
+INSERT INTO TipoTerritorio (nombre) VALUES ('Pais'), ('Provincia'), ('Localidad'), ('Ciudad');
+
+INSERT INTO TipoEleccion (nombre) VALUES ('Presidente'), ('Gobernador'), ('Intendente'), ('Plebiscito');
